4bb719bad4c9c8c857b7cf11ae1b4357a9186478
Myekman <my@traningstemplet.se>
2023-07-03 13:32:19 +0000

    add ALLOWED_HOST to run on heroku

--- bluebellceramics/settings.py
+++ bluebellceramics/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-myekman-bluebellceramic-4uoknwf0o0g.ws-eu101.gitpod.io', 'createceramics.herokuapp.com']
+ALLOWED_HOSTS = ['8000-myekman-bluebellceramic-4uoknwf0o0g.ws-eu101.gitpod.io', 'createceramics-1278ee3bf8cf.herokuapp.com']
 
 
 # Application definition
