855ecdceb60ca1e8f3b6c70ec3f4e32fd5e2df7f
Myekman <my@traningstemplet.se>
2023-07-06 10:51:39 +0000

    install allauth files to workspace

--- createceramics/forms.py
+++ createceramics/forms.py
@@ -9,28 +9,3 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 
-# class BookingForm(forms.ModelForm):
-#     """This class generates a form based on the fields in the
-#     Booking model, except for the User field.
-#     It will then be displayed in the Booking template
-#     in order for the user to make their booking using the form.
-#     """
-#     name = forms.CharField(
-#         widget=forms.TextInput(attrs={'placeholder': 'Name'}),
-#     )
-
-#     email = forms.EmailField(
-#         widget=forms.TextInput(attrs={'placeholder': 'name@example.com'}),
-#     )
-
-#     phone = forms.IntegerField(
-#         required=False,
-#         widget=forms.TextInput(attrs={'placeholder': '+123456789'}),
-#     )
-
-#     class Meta:
-#         model = Booking
-#         exclude = ('user', )
-#         widgets = {
-#             'date': DateInput(),
-#         }
